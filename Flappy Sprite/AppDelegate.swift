7052bcfbcfd8a10dd8a0944469fe8f0f20915cb2
Mark Lifshits <limar.go@gmail.com>
2019-10-19 21:58:17 +0300

    Initial Commit

---
+++ Flappy Sprite/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Flappy Sprite
+//
+//  Created by Mark Lifshits on 19/10/2019.
+//  Copyright © 2019 Mark Lifshits. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ Flappy Sprite/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Flappy Sprite
+//
+//  Created by Mark Lifshits on 19/10/2019.
+//  Copyright © 2019 Mark Lifshits. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
